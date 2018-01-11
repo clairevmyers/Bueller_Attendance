baeba7ba22f1c6b027879d0e5ddfc18c3c129fcb
CLAIRE MYERS <764779@ehs-s-232-36.local>
2018-01-11 12:35:10 -0600

    Group Class 1-11-18 CM

--- Bueller Attendance/StudentClass.swift
+++ Bueller Attendance/StudentClass.swift
@@ -5,9 +5,7 @@
 //  Created by Grace Hansen on 1/9/18.
 //  Copyright © 2018 District196. All rights reserved.
 //
-
 import UIKit
-
 class StudentClass: NSObject {
     
     var FirstName = ""
@@ -37,8 +35,6 @@ class StudentClass: NSObject {
     }
     
 }
-
 var studentList: Array<StudentClass> = Array()
 var Grace = StudentClass(First: "Grace", Last: "Hansen", ID: "762954", Year: 9)
-
 
