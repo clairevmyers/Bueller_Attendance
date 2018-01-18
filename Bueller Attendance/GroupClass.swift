d33b8cf53e2c2280cb95da361e4c812744a375fd
gracehansen <grace@gracehansen.com>
2018-01-18 11:17:30 -0600

    its been a while... GH

--- Bueller Attendance/GroupClass.swift
+++ Bueller Attendance/GroupClass.swift
@@ -2,12 +2,30 @@
 //  GroupClass.swift
 //  Bueller Attendance
 //
-//  Created by MYERS, CLAIRE on 1/11/18.
+//  Created by Grace Hansen on 1/11/18.
 //  Copyright © 2018 District196. All rights reserved.
 //
 
 import UIKit
 
-class GroupClass: NSObject {
+
+class GroupClass {
+    
+    var Name = ""
+    var studentList = [StudentClass]()
+    var checkInDuration = 0.0
+    
+    init()
+    {
+        
+    }
+    
+    init(TempName: String, time: Double)
+    {
+        Name = TempName
+        checkInDuration = time
+    }
+    
+    
 
 }
