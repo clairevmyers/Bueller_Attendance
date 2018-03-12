5416db6e4a919ee904c54fc08812308e4e32ad8d
Claire <claire@ehs-s-xlib-102.local>
2018-03-12 11:50:55 -0500

    Created Summary Page CM

---
+++ Bueller Attendance/SummaryPage.swift
@@ -0,0 +1,35 @@
+//
+//  SummaryPage.swift
+//  Bueller Attendance
+//
+//  Created by Claire on 3/12/18.
+//  Copyright © 2018 District196. All rights reserved.
+//
+
+import UIKit
+
+class SummaryPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
