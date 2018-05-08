c6921fce18a10e87c01f7fb19d805347c0f6b722
gracehansen <grace@gracehansen.com>
2018-05-08 11:22:21 -0500

    saving date

--- Bueller Attendance/globalHolder.swift
+++ Bueller Attendance/globalHolder.swift
@@ -11,11 +11,13 @@ class globalHolder : Codable
 {
     var groupD = groupDict
     var groupN = groupNames
+    var mD = monthDay
     
     func archive(fileName: String)
     {
         groupD = groupDict
         groupN = groupNames
+        mD = monthDay
         let documentsDirectory = FileManager().urls(for: .documentDirectory,
                                                     in: .userDomainMask).first!
         let archiveURL = documentsDirectory.appendingPathComponent(fileName)
@@ -60,6 +62,8 @@ class globalHolder : Codable
                 groupDict = groupD
                 groupN = recoveredData.groupN
                 groupNames = groupN
+                mD = recoveredData.mD
+                monthDay = mD
                 
             }
             catch
