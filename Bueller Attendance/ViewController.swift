526dd33a3543a4cff1c2bd1e1a1d8589875ed9fd
Claire <claire@ehs-s-xlib-102.local>
2018-05-09 11:33:57 -0500

    Student A retrieved CM

--- Bueller Attendance/ViewController.swift
+++ Bueller Attendance/ViewController.swift
@@ -33,12 +33,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
     {
         gD.restore(fileName: "Group D")
+        StudentA.restore(fileName: "StudentA")
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
     
     required init?(coder aDecoder: NSCoder)
     {
         gD.restore(fileName: "Group D")
+        StudentA.restore(fileName: "StudentA")
         super.init(coder: aDecoder)
     }
 
